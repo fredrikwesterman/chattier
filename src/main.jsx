3da334b6f76cda46ede5e0fecdda717e9bbac6b3
Fredrik Westerman <fredrikwesterman9@gmail.com>
2024-08-14 13:44:48 +0200

    smash

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "../src/Comps/App";
+import App from "./Comps/App";
+import "./Style/index.css";
 
 createRoot(document.getElementById("root")).render(<App />);
